d6ff2b30dafe6183b11f84846213d7799799def8
Olawale06 <bamideleolawale13@gmail.com>
2022-10-17 01:47:34 +0100

    Initial commit

---
+++ assets/js/mine.js
@@ -0,0 +1,9 @@
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
+
+
+document.querySelector("counter").counterUp({
+    delay: 10,
+    time: 1000
+});
+
